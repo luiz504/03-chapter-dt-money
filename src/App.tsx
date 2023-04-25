2c60d11fcd9f79935c1185e627e6a3b918b7a01a
Luiz <l.bueno@xsensors.ai>
2023-04-25 11:24:09 -0300

    chore: theme and global styles implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import styled, { ThemeProvider } from 'styled-components'
+
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyles } from './styles/global'
+
+const Hello = styled.div``
+
 export function App() {
-  return <div>HEllo Word</div>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Hello>HEllo Word</Hello>
+      <GlobalStyles />
+    </ThemeProvider>
+  )
 }
